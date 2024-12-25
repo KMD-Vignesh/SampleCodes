8eee8b16cf05307f82b102d79571af7177937331
KMD-Vignesh <KMD.Vignesh@outlook.com>
2024-12-25 21:16:11 +0530

    new changes

--- src/read_intmi.py
+++ src/read_intmi.py
@@ -1,4 +1,5 @@
 import os
+from typing import LiteralString
 import pandas as pd
 
 def get_int_files(base_dir: str) -> dict[str, dict[str, pd.DataFrame]]:
@@ -18,13 +19,18 @@ def get_int_files(base_dir: str) -> dict[str, dict[str, pd.DataFrame]]:
         csv_files_with_ctime.sort(key=lambda x: x[2])
         for file_name, file_path, _ in csv_files_with_ctime:
             df: pd.DataFrame = pd.read_csv(filepath_or_buffer=file_path)
-            csv_files_data[file_name] = df
+            csv_content: LiteralString = f"""<a href='#' onclick=\"openPopup('{df.to_html(index=False, border=1)}')\">View Table</a>"""
+            csv_files_data[str(file_name).split(".csv")[0]] = csv_content
         testcase_data[testcase_folder] = csv_files_data
     return testcase_data
 
-test_int_data: dict[str, dict[str, pd.DataFrame]] = get_int_files(base_dir="data")
-test_step_data: dict = {}
-for key, value in test_int_data.items():
-    print(key)
-    for key1, value1 in value.items():
-        print(key1)
+def get_int_data(base_dir: str) -> dict[str, list[str]]:
+    test_int_data: dict[str, dict[str, pd.DataFrame]] = get_int_files(base_dir=base_dir)
+    test_steps_data: dict[str, list[str]] = {}
+    for key, value in test_int_data.items():
+        ts_li: list = []
+        for key1, value1 in value.items():
+            ts_li.append(key1)
+            ts_li.append(value1)
+        test_steps_data[key] = ts_li
+    return test_steps_data
