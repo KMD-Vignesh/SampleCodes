d50f73aaa8b0e7266d97bfeb752f2ad7cd04b434
Vignesh D <KMD.Vignesh@outlook.com>
2025-02-20 23:34:24 +0530

    Update jsstyle.py

--- src/jsonweb/jsstyle.py
+++ src/jsonweb/jsstyle.py
@@ -1,22 +1,26 @@
 import json
 
-def highlight_json(json_data):
-    def process_value(value):
+def highlight_json(json_data, indent=4):
+    # Define newline and indentation
+    newline = "\n"
+    space = " " * indent  # 4 spaces for indentation
+
+    def process_value(value, level):
         if isinstance(value, dict):
-            return format_dict(value)
+            return format_dict(value, level + 1)
         elif isinstance(value, list):
-            return format_list(value)
+            return format_list(value, level + 1)
         else:
             return f'<span class="json-value">{json.dumps(value)}</span>'
 
-    def format_dict(data):
+    def format_dict(data, level):
         items = []
         for key, value in data.items():
-            items.append(f'<span class="json-key">"{key}"</span>: {process_value(value)}')
-        return f'<span class="json-bracket-curl">{{</span>{", ".join(items)}<span class="json-bracket-curl">}}</span>'
+            items.append(f'{space * level}<span class="json-key">"{key}"</span>: {process_value(value, level)}')
+        return f'<span class="json-bracket-curl">{{</span>{newline}{f",{newline}".join(items)}{newline}{space * (level - 1)}<span class="json-bracket-curl">}}</span>'
 
-    def format_list(data):
-        items = [process_value(item) for item in data]
-        return f'<span class="json-bracket-source">[</span>{", ".join(items)}<span class="json-bracket-source">]</span>'
+    def format_list(data, level):
+        items = [f'{space * level}{process_value(item, level)}' for item in data]
+        return f'<span class="json-bracket-source">[</span>{newline}{f",{newline}".join(items)}{newline}{space * (level - 1)}<span class="json-bracket-source">]</span>'
 
-    return process_value(json_data)
+    return process_value(json_data, level=0)
